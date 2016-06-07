9a953e855d512fd2938271f1482d53bab6c00104
Neha <neha@pstakecare.com>
2016-06-07 17:00:04 +0530

    Add config folder

---
+++ features/support/config/constants.rb
@@ -0,0 +1,4 @@
+REGISTRATION_ID = 'APA91bEx8iHcjDef_s2CiOrmpqchJqupTuc3hA9P3-4JIUeVhm2iJ1tmIyY0Nnsmrpnp9gVBs3z8CEecqpSMOGi69DsPLQNraQQmddpeLWR-VxeWKJqFI-T0aNOlKwOUXI0faIFLkz1Z'
+DEVICE_ID = '9fc65c75b90f469'
+PLATFORM = 'android'
+API_HOST_NAME = 'http://staging.pstakecare.com'
