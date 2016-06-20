c5e4497355bd2e0036c8916becc947750943bccb
Neha <neha@pstakecare.com>
2016-06-20 15:47:46 +0530

    Modify login feature

--- features/api/api_login.feature
+++ features/api/api_login.feature
@@ -3,7 +3,6 @@ Feature: Test Login in B2C App
 User should be able to login, log out via Facebook/ Google and normal sign in
 User should be able to sign in
 	Scenario Outline: Login Functionality negative test cases
-	    Given User is on android platform
 	    When User requests with "username" and "password" for signing into the app
 	    Then Appropriate Response is received with status-code
 		Examples:
--- features/step_definitions/api/api_login_steps.rb
+++ features/step_definitions/api/api_login_steps.rb
@@ -1,13 +1,9 @@
-Given(/^User is on android platform$/) do
-  
-end
 
 When(/^User requests with "([^"]*)" and "([^"]*)" for signing into the app$/) do |username, password|
   @app.login.request_login_sessions(username, password)
 end
 
 Then(/^Appropriate Response is received with status\-code$/) do
-	#binding.pry
   expect(@app.login.session_response).to eq("Invalid Username/Password")
   expect(@app.login.session_code).to eq(401)
 end
