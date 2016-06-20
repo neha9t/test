ca37a33700aca820f529504a79000bf3cc2ee48e
Neha <neha@pstakecare.com>
2016-06-20 15:46:04 +0530

    Modify Application.rb

--- features/support/pages/application.rb
+++ features/support/pages/application.rb
@@ -3,5 +3,21 @@ module Pages
     def login
 	    @login_page ||= Pages::Api::LoginPage.new
 	  end
+
+	  def home
+	  	@home_page ||= Pages::Api::HomePage.new
+	  end
+
+	  def home_search
+			@home_page_search ||=Pages::Api::HomePageSearch.new
+	  end
+
+	  def view_more
+	  	@view_more_search ||= Pages::Api::ViewMore.new
+	  end
+
+	  def summary
+	  	@summary ||=Pages::Api::SummaryPage.new
+	  end
 	end
 end
