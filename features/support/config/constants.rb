074e3330ce3d92ee239dee0efb8677319bd9edd4
Neha <neha@pstakecare.com>
2016-07-19 18:29:34 +0530

    Add 2 new constants Authentication_Token and Api_Host_Name

--- features/support/config/constants.rb
+++ features/support/config/constants.rb
@@ -2,3 +2,4 @@ REGISTRATION_ID = 'APA91bEx8iHcjDef_s2CiOrmpqchJqupTuc3hA9P3-4JIUeVhm2iJ1tmIyY0N
 DEVICE_ID = '9fc65c75b90f469'
 PLATFORM = 'android'
 API_HOST_NAME = 'http://staging.pstakecare.com'
+AUTHORIZATION_TOKEN = 'Token token=x2V2b1gxu1pjL1zWYyU4'
